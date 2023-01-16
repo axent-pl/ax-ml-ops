9545db42e88306ed47613a41023feb80ffc93ea9
Pawel Gasiorowski <pawelgasiorowski@ccs.nl>
2023-01-16 07:34:23 +0100

    Added volumes; Added more logging to experiment

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ax-ml-ops
 
 ## Todos
-* add jupyter notebook
+* add devpi server https://devpi.net/docs/devpi/devpi/latest/+doc/index.html
+* public axent package to devpi server
+* add jupyter notebook with axent package
 * log model as a "final" run of the experiment
--- services/experiment/experiments/spaceship-titanic/experiment.py
+++ services/experiment/experiments/spaceship-titanic/experiment.py
@@ -131,6 +131,7 @@ if __name__ == '__main__':
 
     df_train, df_test = load_and_transform_data()
     X_train, y_train = get_xy(df_train)
+    X_cols, y_col = get_xy_cols(df_train)
 
     @mlflc.track_in_mlflow()
     def objective(trial):
@@ -146,16 +147,25 @@ if __name__ == '__main__':
 
         kf = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 7)
         scores = cross_validate(model, X_train, y_train, cv = kf, scoring = "accuracy", n_jobs = -1, return_train_score=True)
-
-        # mlflow.catboost.log_model(model, 'model')
         
-        mlflow.log_metric("train_score_min", scores["train_score"].min())
-        mlflow.log_metric("train_score_mean", scores["train_score"].mean())
-        mlflow.log_metric("train_score_max", scores["train_score"].max())
+        mlflow.log_metric("cv_train_min_score", scores["train_score"].min())
+        mlflow.log_metric("cv_train_mean_score", scores["train_score"].mean())
+        mlflow.log_metric("cv_train_max", scores["train_score"].max())
 
-        mlflow.log_metric("test_score_min", scores["test_score"].min())
-        mlflow.log_metric("test_score_mean", scores["test_score"].mean())
-        mlflow.log_metric("test_score_max", scores["test_score"].max())
+        mlflow.log_metric("cv_test_min_score", scores["test_score"].min())
+        mlflow.log_metric("cv_test_mean_score", scores["test_score"].mean())
+        mlflow.log_metric("cv_test_max_score", scores["test_score"].max())
+
+        # log datasets
+        df_train[X_cols+[y_col]].to_csv(f'train_{trial.number}.csv')
+        df_test[X_cols].to_csv(f'test_{trial.number}.csv')
+        mlflow.log_artifact(f'train_{trial.number}.csv','dataset')
+        mlflow.log_artifact(f'test_{trial.number}.csv','dataset')
+
+        # fit and log model
+        model.fit(X_train, y_train)
+        mlflow.log_metric("train_score", model.score(X_train, y_train))
+        mlflow.catboost.log_model(model, 'model')
 
         return scores["test_score"].mean()
     
@@ -165,4 +175,4 @@ if __name__ == '__main__':
         direction = "maximize",
         storage=f'postgresql+psycopg2://optuna:{os.environ.get("DB_OPTUNA_PASS")}@db/optuna'
     )
-    study.optimize(objective, n_trials=10, callbacks=[mlflc])
+    study.optimize(objective, n_trials=100, callbacks=[mlflc])
