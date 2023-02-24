27c8df57182fcb2da4244718fe86ebb916f5cc04
Pawel Gasiorowski <pawelgasiorowski@ccs.nl>
2023-02-24 12:19:16 +0100

    Added docs to ColumnTransformer

--- pipelines/kaggle_spaceship_titanic/common/transformer/column_transformer.py
+++ pipelines/kaggle_spaceship_titanic/common/transformer/column_transformer.py
@@ -3,13 +3,41 @@ from typing import Union, List
 from pandas.core.frame import DataFrame
 
 class ColumnTransformer:
-    
+    """
+    A class that can be used to apply a transformation to a subset of columns in a pandas DataFrame.
+
+    Attributes
+    ----------
+    _fitted: bool
+        a boolean that indicates whether the transformer has been fitted to the data
+    _fit_once: bool
+        a boolean that indicates whether the transformer should only be fitted once
+    _transformer: callable
+        the transformer that should be applied to the selected columns
+    _columns: List[Union[str,Pattern]]
+        a list of column names or regular expressions that define the subset of columns to be transformed
+    _calculated_columns: List[str]
+        a list of column names that have been selected for transformation
+
+    Methods
+    -------
+    __init__(self, transformer, columns:List[Union[str,Pattern]]=[], fit_once=True)
+        initializes the ColumnTransformer object with the transformer, columns to be transformed,
+        and whether the transformer should be fit once
+    _calculate_columns(self, X:DataFrame) -> List[str]
+        selects the columns to be transformed based on the _columns attribute
+    fit(self, X:DataFrame, y=None)
+        fits the transformer to the selected columns in the input DataFrame
+    transform(self, X:DataFrame, y=None)
+        applies the fitted transformer to the selected columns in the input DataFrame
+    """
+
     def __init__(self, transformer, columns:List[Union[str,Pattern]]=[], fit_once=True):
-        self._fitted = False
-        self._fit_once = False
+        self._fitted:bool = False
+        self._fit_once:bool = False
         self._transformer = transformer
-        self._columns = columns
-        self._calculated_columns = []
+        self._columns:List[Union[str,Pattern]] = columns
+        self._calculated_columns:List[str] = []
         
     def _calculate_columns(self, X:DataFrame) -> List[str]:
         _columns = []
